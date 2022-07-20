9a2770079d5fe7fd4553efe8e5fc028a246eede7
igortargino <igortargino01@gmail.com>
2022-07-19 22:16:15 -0300

    feat: add params

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,6 +4,7 @@ module.exports = {
   listUsers(request, response) {
     const { order } = request.query;
 
+
     const sortUsers = users.sort((a, b) => {
       if(order === 'desc') {
         return a.id < b.id ? 1 : - 1;
@@ -14,5 +15,19 @@ module.exports = {
 
     response.writeHead(200, { 'Content-type': 'application.json'});
     response.end(JSON.stringify(sortUsers));
-  }
+  },
+
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { 'Content-type': 'application.json'});
+      response.end(JSON.stringify({ error: 'User not found'}));
+    } else {
+      response.writeHead(200, { 'Content-type': 'application.json'});
+      response.end(JSON.stringify({ user }));
+    }
+  },
 }
